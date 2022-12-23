778dee2f63df1d83202563c8f200941efe434f78
Hadi <hadirezaei2828@gmail.com>
2022-12-24 00:43:58 +0330

    Create Sign up styles

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,33 +5,35 @@ const SignUp = () => {
     return (
         <div className={styles.container}>
             <form className={styles.formContainer}>
-                <h1>SignUp</h1>
+                <h1 className={styles.header}>SignUp</h1>
                 <div className={styles.formField}>
                     <label>Name</label>
-                    <input type="text"/>
+                    <input type="text" className={styles.formInput} />
                     <span>Name is required</span>
                 </div>
                 <div className={styles.formField}>
                     <label>Email</label>
-                    <input type="email"/>
+                    <input type="email" className={styles.formInput} />
                     <span>Email is required</span>
                 </div>
                 <div className={styles.formField}>
                     <label>Password</label>
-                    <input type="password"/>
+                    <input type="password" className={styles.formInput} />
                     <span>Password is required</span>
                 </div>
                 <div className={styles.formField}>
                     <label>Confirm Password</label>
-                    <input type="password"/>
+                    <input type="password" className={styles.formInput} />
                     <span>Confirm the Password</span>
                 </div>
                 <div className={styles.formField}>
-                    <label>I accept terms of privacy policy</label>
-                    <input type="checkbox"/>
+                    <div className={styles.checkBoxContainer}>
+                        <label>I accept terms of privacy policy</label>
+                        <input className={styles.checkbox} type="checkbox" />
+                    </div>
                     <span>Accept our regulations</span>
                 </div>
-                <div className={styles.formButton}>
+                <div className={styles.formButtons}>
                     <a href='#'>Login</a>
                     <button type="submit">Sign Up</button>
                 </div>
