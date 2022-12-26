51927a71217e2e42d4f3dccfc74ea6d33bb8af12
Hadi <hadirezaei2828@gmail.com>
2022-12-27 00:33:51 +0330

    Create Responsive Navber

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Navbar.module.css";
 import logo from "../images/structure.png";
 import { Link } from 'react-router-dom';
+import { bars } from 'react-icons-kit/fa/bars';
+import { Icon } from 'react-icons-kit';
+import { close } from 'react-icons-kit/fa/close';
+
+// `${toggle ? styles.listshow & styles.list : styles.list}`
 
 const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
     return (
         <div className={styles.navbar}>
             <div className={styles.logo}>
                 <Link to="/"><img src={logo} alt="logo" /></Link>
             </div>
-            <div className={styles.list}>
+            <div className={`${toggle ? `${styles.active} ${styles.list}` : styles.list}`} >
                 <ul>
                     <Link to="/"><li>Home</li></Link>
                     <Link to="/products"><li>Products</li></Link>
@@ -20,6 +28,12 @@ const Navbar = () => {
                         <Link to="/login"><span>Login</span></Link>
                     </li>
                 </ul>
+            </div>
+            <div className={styles.bars}>
+                {toggle ?
+                    <Icon onClick={() => setToggle(!toggle)} icon={close} /> :
+                    <Icon onClick={() => setToggle(!toggle)} icon={bars} />
+                }
             </div>
         </div>
     );
