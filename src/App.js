947cad77cda7db4380ba144ee012c074bbf45201
Hadi <hadirezaei2828@gmail.com>
2022-12-28 18:10:49 +0330

    add About Us

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Aboutus from "./components/Aboutus";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/signup" element={<SignUp />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/" element={<Navigate to="/signup" />}/>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/" element={<Navigate to="/signup" />} />
       </Routes>
     </div>
   );
