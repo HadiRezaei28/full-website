d443a81403b4c53a7501ec5cb08c6dcfea261d01
Hadi <hadirezaei2828@gmail.com>
2023-01-08 12:50:22 +0330

    use cartSlice in products

--- src/app/features/cart/cartSlice.js
+++ src/app/features/cart/cartSlice.js
@@ -17,52 +17,36 @@ const cartSlice = createSlice({
                     ...action.payload,
                     quantity: 1
                 });
-                return {
-                    ...state,
-                    selectedItems: [...state.selectedItems]
-                }
+                state.selectedItems = [...state.selectedItems];
             }
         },
         RemoveItem: (state, action) => {
             const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id);
-            return {
-                ...state,
-                selectedItems: [...newSelectedItems]
-            }
+            state.selectedItems = [...newSelectedItems];
         },
         Increase: (state, action) => {
             const indexI = state.selectedItems.findIndex(item => item.id === action.payload.id);
             state.selectedItems[indexI].quantity++;
-            return {
-                ...state
-            }
         },
         Decrease: (state, action) => {
             const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id);
             state.selectedItems[indexD].quantity--;
-            return {
-                ...state
-            }
         },
-        Checkout: () => {
-            return {
-                selectedItems: [],
-                itemsCounter: 0,
-                total: 0,
-                checkout: true
-            }
+        Checkout: (state) => {
+            state.selectedItems = [];
+            state.itemsCounter = 0;
+            state.total = 0;
+            state.checkout = true
         },
-        Clear: () => {
-            return {
-                selectedItems: [],
-                itemsCounter: 0,
-                total: 0,
-                checkout: false
-            }
+        Clear: (state) => {
+            state.selectedItems = [];
+            state.itemsCounter = 0;
+            state.total = 0;
+            state.checkout = false
         }
     }
 })
 
 
-export const {AddItem, RemoveItem, Increase, Decrease, Checkout, Clear} = cartSlice.actions;
+export const { AddItem, RemoveItem, Increase, Decrease, Checkout, Clear } = cartSlice.actions;
 export default cartSlice.reducer;
--- src/app/features/products/SingleProduct.js
+++ src/app/features/products/SingleProduct.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { shorten } from '../../../functions/functions';
+import { shorten, isInCart, quantityCount } from '../../../functions/functions';
+import { AddItem, RemoveItem, Increase, Decrease } from '../cart/cartSlice';
+import { FaTrashAlt } from "react-icons/fa";
 
-const SingleProduct = ({product}) => {
+
+const SingleProduct = ({ product }) => {
+
+    const dispatch = useDispatch();
+    const state = useSelector(state => state.cart);
+
     return (
         <div>
-            <img src={product.image} alt='product' style={{width: "200px"}}/>
+            <img src={product.image} alt='product' style={{ width: "200px" }} />
             <h3>{shorten(product.title)}</h3>
             <p>{product.price} $</p>
             <div>
                 <Link to={`/products/${product.id}`}>Detail</Link>
                 <div>
-                    <button>Add to card</button>
-                    <button></button>
+                    {quantityCount(state, product.id) === 1 && <button onClick={() => dispatch(RemoveItem(product))}><FaTrashAlt /></button>}
+                    {quantityCount(state, product.id) > 1 && <button onClick={() => dispatch(Decrease(product))}>-</button>}
+                    {quantityCount(state, product.id) > 0 && quantityCount(state, product.id)}
+                    {
+                        isInCart(state, product.id) ?
+                            <button onClick={() => dispatch(Increase(product))}>+</button> :
+                            <button onClick={() => dispatch(AddItem(product))}>Add to Cart</button>
+                    }
+
                 </div>
             </div>
         </div>
